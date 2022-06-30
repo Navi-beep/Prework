21a2279204f55b6a16204f3022f07d12a4046b5f
Navi-beep <estawars30@gmail.com>
2022-06-29 20:39:42 -0500

    Add files via upload

--- video_notes.py
+++ video_notes.py
@@ -1,67 +1,31 @@
-my_dict = {}
-my_dict = dict()
+# The while loop
+#while Boolean or Boolean Exp:
+    #code block
+    # to run while
+    #condition is true
 
-my_dict = {
-    "key":"value"
-}
+#You must be over 5 feet to ride my ride
+# I have a magic potion that will let you grow one inch every tme you use it 
 
-my_dict = dict(key="value")
+#height = int(input("What is your height in inches? "))
 
-# a dictionary is a collection of key value pairs
+#while height < 60:
+    #height += 1
+   # if height < 58:
+    #    continue
+    #print(f"You are {height} inches tall \n and too short to ride!")
+   # print("Drink my magic potion")
+    
+#print("Thanks for coming")
 
-steve = {
-    "name" : "Steve", #Str
-    "weight" : 155.5, #flt
-    "height": 70, #int
-    "foods": ["country fried steak", "fried chicken", "collards"],
-    "ice cream": {
-        "vanilla": False,
-        "chocolate": True,
-        "coffee": False,
-    },
-    10 : "this has an integer key"
-}
-# name_of_dict[KEY]
-print(steve["ice cream"]["vanilla"])
-print(type(steve["ice cream"]["vanilla"]))
-
-print(steve.get("candies")) #returns NONE if nothing there
-print(steve.get("candies", "No Candy List")) # will return second argument if nothing there
-
-
-my_list = [1,2,3]
-my_list[1] = "WOW"
-print(my_list)
-
-# add key value pair to steve list
-steve.update({"candies" : ["snickers", "mars", "m&ms"]})
-print(steve)
-
-del steve["candies"]
-print(steve)
-
-
-for key in steve:
-    print(key)
-    print(type(key))
-    print(steve[key])
-
-print(steve.values()) # loop through values
-for value in steve.values():
-    print(value)
-    print(type(value))
-
-print(steve.items()) #
-for key , value in steve.items():
-    print(key, end = " ")
-    print(value)
-
-for key , value in steve.items():
-    if isinstance(value,list): #return whether an object is an instance of a class or of a subclass thereof. 
-        print(f"The list is at {key}")
-
-
-
-
-
+while True:
+    response = input("what do you want to do? Say hi [h], Say goodbye [g], or Quit? [q]")
+    if response.lower() == "h":
+        print("hello")
+    elif response.lower()=="g":
+        print("goodbye")
+    elif response.lower()=="q":
+        break
+    else:
+        print("invalid option")
 
